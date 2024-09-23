2c91628b2b04ba49b64493f36552164f34bbca70
nan mu <mu.nan.11@outlook.com>
2024-09-23 21:21:03 +0800

    feat: 添加常用命令别名

--- home.nix
+++ home.nix
@@ -18,6 +18,16 @@
     userEmail = "mu.nan.11@outlook.com";
   };
 
+  # bash 相关配置
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      ee = "eza -lh";
+      reos = "echo \"In addition to reloading the nixos configuration, .bashrc is also reloaded. This can be a dangerous behavior\" && sudo nixos-rebuild switch --flake . && . ~/.bashrc";
+    };
+  };
+
+
   home.stateVersion = "24.05";
 
   programs.home-manager.enable = true;
